c0902e96877f4675f6c3b59b6c05e7fa3ecc71bc
tkzc00 <2238983030@qq.com>
2023-05-20 16:30:55 +0800

    创建槽函数

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,99 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_new_file_triggered()
+{
+
+}
+
+
+void MainWindow::on_open_file_triggered()
+{
+
+}
+
+
+void MainWindow::on_save_file_triggered()
+{
+
+}
+
+
+void MainWindow::on_save_as_triggered()
+{
+
+}
+
+
+void MainWindow::on_paste_triggered()
+{
+
+}
+
+
+void MainWindow::on_cut_triggered()
+{
+
+}
+
+
+void MainWindow::on_copy_triggered()
+{
+
+}
+
+
+void MainWindow::on_bolder_triggered()
+{
+
+}
+
+
+void MainWindow::on_italic_triggered()
+{
+
+}
+
+
+void MainWindow::on_underline_triggered()
+{
+
+}
+
+
+void MainWindow::on_font_triggered()
+{
+
+}
+
+
+void MainWindow::on_about_triggered()
+{
+
+}
+
+
+void MainWindow::on_undo_triggered()
+{
+
+}
+
+
+void MainWindow::on_redo_triggered()
+{
+
+}
+
+
+void MainWindow::on_exit_triggered()
+{
+
+}
+
+
+void MainWindow::on_print_triggered()
+{
+
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,39 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_new_file_triggered();
+
+    void on_open_file_triggered();
+
+    void on_save_file_triggered();
+
+    void on_save_as_triggered();
+
+    void on_paste_triggered();
+
+    void on_cut_triggered();
+
+    void on_copy_triggered();
+
+    void on_bolder_triggered();
+
+    void on_italic_triggered();
+
+    void on_underline_triggered();
+
+    void on_font_triggered();
+
+    void on_about_triggered();
+
+    void on_undo_triggered();
+
+    void on_redo_triggered();
+
+    void on_exit_triggered();
+
+    void on_print_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
